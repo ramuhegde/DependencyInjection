96e789cf4e32f0f5753ed6895f411eca613da25e
Ramu Hegde <ramuhegde18@gmail.com>
2024-03-24 17:07:12 +0530

    Bumped the Hilt Dependency version to 2.48 to fix Kotlin related gradle issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ dependencies {
     // implementation("com.google.dagger:dagger:2.48")
     // kapt("com.google.dagger:dagger-compiler:2.48")
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
