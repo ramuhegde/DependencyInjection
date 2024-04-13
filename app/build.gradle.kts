ebf6e7b16c1696ad110adeb7b18706ed1de9d75b
ramachandra-hegde_inmobi <ramachandra.hegde@glance.com>
2024-04-13 08:40:08 +0530

    Migrated to version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
-    id("androidx.room")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
+    alias(libs.plugins.android.room)
 }
 
 android {
@@ -51,30 +51,29 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.android.ktx)
+    implementation(libs.android.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraint.layout)
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
     // Dagger2
     // implementation("com.google.dagger:dagger:2.48")
     // kapt("com.google.dagger:dagger-compiler:2.48")
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation(libs.hilt.andriod)
+    kapt(libs.hilt.compiler)
     // For ViewModel and Lifecycles
-    implementation ("androidx.activity:activity-ktx:1.8.2")
+    implementation (libs.activity.ktx)
     // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.android.room)
+    kapt(libs.room.compiler)
+    implementation(libs.room.ktx)
     // Preferences DataStore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation(libs.preference.datastore)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.android.test.junit)
+    androidTestImplementation(libs.android.test.espresso)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("androidx.room") version "2.6.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.android.room) apply false
+    alias(libs.plugins.jetbrains.kotlin.kapt) apply false
 }
