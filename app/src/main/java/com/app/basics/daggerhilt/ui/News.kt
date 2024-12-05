dd006734848f3ea2579f723fef5b4e04a2f27c2e
ramachandra-hegde_inmobi <ramachandra.hegde@glance.com>
2024-12-05 14:59:36 +0530

    Added padding for card

--- app/src/main/java/com/app/basics/daggerhilt/ui/News.kt
+++ app/src/main/java/com/app/basics/daggerhilt/ui/News.kt
@@ -1,13 +1,22 @@
 package com.app.basics.daggerhilt.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NewsCard(title: String, description: String) {
-    Card {
-        Text(text = title)
-        Text(text = description)
+    Card (modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)) {
+        Column (modifier = Modifier.padding(8.dp)) {
+            Text(text = title)
+            Text(text = description)
+        }
     }
 }
